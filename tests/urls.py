c0ddc744f62ab944c1ee13d5ff667a162dd2d2b1
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-11-14 18:10:05 +0300

    Add Urls class with API endpoint constants

---
+++ tests/urls.py
@@ -0,0 +1,5 @@
+class Urls:
+    BASE_URL = "https://stellarburgers.education-services.ru"
+    CREATE_USER_URL = f"{BASE_URL}/api/auth/register"
+    LOGIN_USER_URL = f"{BASE_URL}/api/auth/login"
+    USER_DATA_URL = f"{BASE_URL}/api/auth/user"
