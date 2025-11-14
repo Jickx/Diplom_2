15d2d41b22d1e55ae0b1b326122fef5e16bcf3e0
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-11-14 20:48:14 +0300

    Refactor user creation tests to use UserHelper and improve error handling

--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -1,34 +1,39 @@
 import pytest
 import requests
-from urls import Urls
+
+from data.messages import USER_ALREADY_EXISTS, REQUIRED_FIELDS_ERROR
+from data.urls import Urls
+from http import HTTPStatus
+
+from helpers.user_helper import UserHelper
 
 
 class TestUserCreation:
 
-    def test_create_unique_user_success(self, user_data):
+    def test_create_unique_user_success(self, user):
         """Тест успешного создания уникального пользователя."""
-        response = requests.post(Urls.CREATE_USER_URL, json=user_data)
+        response = UserHelper.create_user(user)
 
-        assert response.status_code == 200
+        assert response.status_code == HTTPStatus.OK
         assert response.json()["success"] is True
         assert "accessToken" in response.json()
 
-    def test_create_existing_user_error(self, user_data):
+    def test_create_existing_user_error(self, user):
         """Тест ошибки при создании пользователя, который уже существует."""
-        requests.post(Urls.CREATE_USER_URL, json=user_data)
+        requests.post(Urls.CREATE_USER_URL, json=user)
 
-        response = requests.post(Urls.CREATE_USER_URL, json=user_data)
+        response = requests.post(Urls.CREATE_USER_URL, json=user)
 
-        assert response.status_code == 403
+        assert response.status_code == HTTPStatus.FORBIDDEN
         assert response.json()["success"] is False
-        assert response.json()["message"] == "User already exists"
+        assert response.json()["message"] == USER_ALREADY_EXISTS
 
     @pytest.mark.parametrize("missing_field", ["email", "password", "name"])
-    def test_create_user_missing_required_field_error(self, user_data, missing_field):
+    def test_create_user_missing_required_field_error(self, user, missing_field):
         """Тест ошибки при отсутствии одного из обязательных полей."""
-        user_data.pop(missing_field)
-        response = requests.post(Urls.CREATE_USER_URL, json=user_data)
+        user.pop(missing_field)
+        response = requests.post(Urls.CREATE_USER_URL, json=user)
 
-        assert response.status_code == 403
+        assert response.status_code == HTTPStatus.FORBIDDEN
         assert response.json()["success"] is False
-        assert response.json()["message"] == "Email, password and name are required fields"
+        assert response.json()["message"] == REQUIRED_FIELDS_ERROR
